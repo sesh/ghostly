c0fa555fa89d04bab85d220529aaa51db00a72fd
Brenton Cleeland <brenton@brntn.me>
2017-02-24 15:21:42 +1100

    Install ghostly with pip, not by cloning the repo

--- pre-fright.sh
+++ pre-fright.sh
@@ -8,9 +8,4 @@ sudo mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
 sudo chmod a+x /usr/local/bin/phantomjs
 
 sudo pip3 install glances
-git clone https://github.com/sesh/ghostly.git
-cd ghostly
-git checkout feature/fright
-
-sudo pip3 install -r requirements.txt
-sudo pip3 install .
+sudo pip3 install git+https://github.com/sesh/ghostly.git
